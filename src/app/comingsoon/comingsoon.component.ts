4114a7c6736b67d1c0a26814d6895f06be31e737
SithumKavinda <it20050344@my.sliit.lk>
2022-11-29 02:34:44 +0530

    Coming soon and Footer components created

---
+++ src/app/comingsoon/comingsoon.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coming-soon',
+  templateUrl: './comingsoon.component.html',
+  styleUrls: ['./comingsoon.component.css'],
+})
+export class ComingsoonComponent {}
