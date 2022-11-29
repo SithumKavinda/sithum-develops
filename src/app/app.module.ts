463bf38c233963b0441878dcf3778d52e99ed899
SithumKavinda <it20050344@my.sliit.lk>
2022-11-29 23:57:27 +0530

    Lottie file added and changed the topic into site still under constructing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { ComingsoonComponent } from './comingsoon/comingsoon.component';
 import { FooterComponent } from './footer/footer.component';
 
+// newly added modules
+import { CommonModule } from '@angular/common';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+export function playerFactory(): any {
+  return import('lottie-web');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ComingsoonComponent,
-    FooterComponent
-  ],
+  declarations: [AppComponent, ComingsoonComponent, FooterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/comingsoon/comingsoon.component.ts
+++ src/app/comingsoon/comingsoon.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'coming-soon',
   templateUrl: './comingsoon.component.html',
   styleUrls: ['./comingsoon.component.css'],
 })
-export class ComingsoonComponent {}
+export class ComingsoonComponent {
+  AnimationOptions = {
+    path: 'assets/lotties/UnderConstructionLottie.json',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // This is the component function that binds to the animationCreated event from the package
+  onAnimate(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+}
