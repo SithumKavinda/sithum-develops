a304e1e79cf8b80f60359a47ff8dcd8a5f08e13b
Sithum Codes <it20050344@my.sliit.lk>
2023-01-06 23:56:48 +0530

    Components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProjectsComponentComponent } from './projects-component/projects-component.component';
+import { QualificationComponentComponent } from './qualification-component/qualification-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'about', component: AboutComponentComponent },
+  { path: 'qualifications', component: QualificationComponentComponent },
+  { path: 'projects', component: ProjectsComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
